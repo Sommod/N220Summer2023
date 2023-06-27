3fada8afa6acb512c3b9f8c20b37f6533fd7cb39
Sommod <Sommod@192.168.0.105>
2023-06-27 17:00:56 -0400

    Edited Template

--- Template/js/app.js
+++ Template/js/app.js
@@ -1,7 +1,23 @@
 /*
-* Name: Josh Moore
-* Assignment: ASSIGNMENT
-* Date: DATE
-* Description:
-* Log: LOG
-*/
+ * Name: Josh Moore
+ * Assignment: ASSIGNMENT
+ * Date: DATE
+ * Description:
+ * Log: LOG
+ */
+
+/**
+ * @method setup
+ * @description Creates a canvas of default 800px by 600px.
+ */
+function setup() {
+    createCanvas(800, 600);
+}
+
+/**
+ * @method draw
+ * @description Function used for using different P5 methods for creating abstract drawings using JS.
+ */
+function draw() {
+    circle(200, 200, 50);
+}
