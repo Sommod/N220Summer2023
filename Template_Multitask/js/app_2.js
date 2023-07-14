338cf02056d1ae2453ffb834a9ca30efc60555f1
Sommod <Sommod@192.168.0.105>
2023-07-14 13:23:53 -0400

    Added Template for multi-task assignments

---
+++ Template_Multitask/js/app_2.js
@@ -0,0 +1,25 @@
+/*
+ * Name: Josh Moore
+ * Assignment: ASSIGNMENT
+ * Date: DATE
+ * Description:
+ * Log: LOG
+ */
+
+/**
+ * @method setup
+ * @description Creates a canvas of default 800px by 600px.
+ */
+function setup() {
+    // Creates a Canvas at the top-left portion of browser
+    // Dimensions: 800px - 600px
+    createCanvas(800, 600);
+}
+
+/**
+ * @method draw
+ * @description Function used for using different P5 methods for creating abstract drawings using JS.
+ */
+function draw() {
+    //TODO: Use method to create different items for drawings
+}
